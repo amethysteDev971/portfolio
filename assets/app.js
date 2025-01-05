3eddb9c98db727e5f24aec5a0f5c149615d9e1f8
amethysteDev971 <amethyste.design@gmail.com>
2025-01-05 15:51:30 -0400

    Fix Mansory

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,32 @@ import './styles/app.css';
 
 // enable the interactive UI components from Flowbite
 import 'flowbite';
+// Importation de Masonry
+import Masonry from 'masonry-layout';
+
+// Initialisation de Masonry sur un conteneur spécifique
+document.addEventListener("DOMContentLoaded", function() {
+  const grid = document.querySelector('.grid_masonry'); // Sélectionner l'élément contenant les éléments à agencer
+
+  // Initialiser Masonry
+  const masonry = new Masonry(grid, {
+    itemSelector: '.grid-item', // Sélectionner chaque élément de la grille
+    columnWidth: '.grid-sizer', // Définir la largeur de la colonne
+    percentPosition: true
+  });
+});
+
+window.onresize = () => {
+    console.log('onresize =>', window.innerWidth);
+}
+
+// window.onload = () => {
+//     const grid = document.querySelector('.grid_masonry'); 
+//     const masonry = new Masonry(grid, {
+//         itemSelector: '.grid-item',
+//         // gutter: 10,
+//         // use element for option
+//         columnWidth: '.grid-sizer',
+//         percentPosition: true
+//     });
+// }
