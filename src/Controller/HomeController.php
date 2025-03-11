6c136180712ee842cad58cbd66dd912118ae2db1
Galoyer Marc <mgaloyer@uneak.fr>
2025-03-11 19:31:15 -0400

    remove APP_ENV

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
@@ -15,5 +18,21 @@ class HomeController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/HomeController.php',
         ]);
+    }
+
+
+    #[Route('/send-email', name: 'send_email')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('	no-reply@amethyste-design.fr')
+            ->to('mgaloyer@uneak.fr')
+            ->subject('Test Mailjet via Symfony Mailer')
+            ->text('Bonjour, ceci est un test d’envoi via Mailjet !')
+            ->html('<p>Bonjour, ceci est un <strong>test</strong> d’envoi via Mailjet !</p>');
+
+        $mailer->send($email);
+
+        return new Response('Email envoyé avec succès !');
     }
 }
