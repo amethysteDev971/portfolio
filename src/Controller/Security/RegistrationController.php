dd73fbbf5cf74e8e30f53220e23261a25f6ea1e2
amethysteDev971 <amethyste.design@gmail.com>
2025-03-11 15:14:08 -0400

    Fixed MAILER Registration FROM

--- src/Controller/Security/RegistrationController.php
+++ src/Controller/Security/RegistrationController.php
@@ -51,7 +51,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('contact@amethyste-design.com', 'Mail'))
+                    ->from(new Address('no-reply@amethyste-design.fr', 'Inscription'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -83,7 +83,7 @@ class RegistrationController extends AbstractController
                 // generate a signed url and email it to the user
                 $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('contact@amethyste-design.com', 'Mail'))
+                    ->from(new Address('no-reply@amethyste-design.fr', 'Inscription'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
